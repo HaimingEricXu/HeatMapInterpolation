eb4b7eff1865c3849633c7276deec9223f5d9d1a
Haiming Xu <haiming19@berkeley.edu>
2020-07-21 17:14:33 -0400

    set up the map and heat map initial data

---
+++ HeatMapInterpolation/HeatMapInterpolation.swift
@@ -0,0 +1,16 @@
+
+//
+//  HeatMapInterpolation.swift
+//  HeatMapFeature
+//
+//  Created by Haiming Xu on 7/20/20.
+//  Copyright © 2020 Haiming Xu. All rights reserved.
+//
+import Foundation
+
+class HeatMapInterpolation {
+    
+    init(dataset: [[Double]]) {
+        
+    }
+}
